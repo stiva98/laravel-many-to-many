6864c2181e2dd5f93b1dfee43a1c4aa3ca03732b
stiva98 <samustiva9810@gmail.com>
2023-09-24 19:14:30 +0200

    ADD destroy()

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -77,6 +77,8 @@ class TechnologyController extends Controller
      */
     public function destroy(Technology $technology)
     {
-        //
+        $technology -> delete();
+
+        return redirect() ->route('admin.technologies.index');
     }
 }
